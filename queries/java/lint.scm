ded2ed2129f97074f6777227b65269bae1e3b55c
Robert Muir <rmuir@apache.org>
2026-02-08 17:34:14 -0500

    honesty

--- queries/java/lint.scm
+++ queries/java/lint.scm
@@ -1,13 +1,17 @@
+;; P0000: hard parsing error
 ((ERROR) @error
  (#set! name "syntax-error")
  (#set! title "Syntax Error")
  (#set! label "parse error here"))
 
+;; P0001: soft parsing error
 ((MISSING) @error
- (#set! name "syntax-missing")
- (#set! title "Syntax Missing")
+ (#set! name "missing-syntax")
+ (#set! title "Missing syntax element")
  (#set! label "missing element here"))
 
+;; P0002: wildcard imports
+;; https://google.github.io/styleguide/javaguide.html#s3.3.1-wildcard-imports
 ((import_declaration
   (asterisk) @error)
  (#set! name "wildcard-import")
