c0f553315b74632eddeba56c2a58aee4125528be
Robert Muir <rmuir@apache.org>
2026-02-08 18:49:24 -0500

    continue: add another lint

--- queries/java/lint.scm
+++ queries/java/lint.scm
@@ -1,17 +1,26 @@
-;; P0000: hard parsing error
+; P0000: hard parsing error
 ((ERROR) @error
  (#set! name "syntax-error")
  (#set! title "Syntax Error")
  (#set! label "parse error here"))
 
-;; P0001: soft parsing error
+; P0001: soft parsing error
 ((MISSING) @error
  (#set! name "missing-syntax")
  (#set! title "Missing syntax element")
  (#set! label "missing element here"))
 
-;; P0002: wildcard imports
-;; https://google.github.io/styleguide/javaguide.html#s3.3.1-wildcard-imports
+; P0002: special escape sequences encoded as octal/hex
+; https://google.github.io/styleguide/javaguide.html#s2.3.2-special-escape-sequences
+((escape_sequence) @error
+ ; BS, TAB, NL
+ (#any-of? @error "\\u0008" "\\010" "\\10" "\\u0009" "\\011" "\\11" "\\u000A" "\\012" "\\12")
+ (#set! name "raw-special-escape")
+ (#set! title "Raw special escape sequence in octal/hex form")
+ (#set! label "raw escape here"))
+
+; P0003: wildcard imports
+; https://google.github.io/styleguide/javaguide.html#s3.3.1-wildcard-imports
 ((import_declaration
   (asterisk) @error)
  (#set! name "wildcard-import")
