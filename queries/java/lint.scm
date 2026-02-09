7e8865f4543f49ff0853e924e83b285247c3dd65
Robert Muir <rmuir@apache.org>
2026-02-09 08:41:10 -0500

    fix

--- queries/java/lint.scm
+++ queries/java/lint.scm
@@ -4,16 +4,16 @@
   (#set! severity "warning")
   (#set! title "Parse Error")
   (#set! label "Parse error here")
-  (#set! help "Correct the invalid java syntax"))
+  (#set! help "Correct the invalid Java syntax"))
 
 ; special escape sequences encoded as octal/hex
 ; https://google.github.io/styleguide/javaguide.html#s2.3.2-special-escape-sequences
 ((escape_sequence) @error
   (#any-of? @error "\\u0008" "\\010" "\\10")
-  (#set! severity "warning")
+  (#set! severity "error")
   (#set! name "raw-special-escape")
   (#set! title "Special escape sequence in octal/hex form")
-  (#set! label "Raw escape here")
+  (#set! label "Raw escape used here")
   (#set! help "Replace the raw escape with \\b")
   (#set! fix "\\b"))
 
@@ -21,10 +21,10 @@
 ; https://google.github.io/styleguide/javaguide.html#s2.3.2-special-escape-sequences
 ((escape_sequence) @error
   (#any-of? @error "\\u0009" "\\011" "\\11")
-  (#set! severity "warning")
+  (#set! severity "error")
   (#set! name "raw-special-escape")
   (#set! title "Special escape sequence in octal/hex form")
-  (#set! label "Raw escape here")
+  (#set! label "Raw escape used here")
   (#set! help "Replace the raw escape with \\t")
   (#set! fix "\\t"))
 
@@ -32,18 +32,40 @@
 ; https://google.github.io/styleguide/javaguide.html#s2.3.2-special-escape-sequences
 ((escape_sequence) @error
   (#any-of? @error "\\u000a" "\\u000A" "\\012" "\\12")
-  (#set! severity "warning")
+  (#set! severity "error")
   (#set! name "raw-special-escape")
   (#set! title "Special escape sequence in octal/hex form")
-  (#set! label "Raw escape here")
+  (#set! label "Raw escape used here")
   (#set! help "Replace the raw escape with \\n")
   (#set! fix "\\n"))
+
+; special escape sequences encoded as octal/hex
+; https://google.github.io/styleguide/javaguide.html#s2.3.2-special-escape-sequences
+((escape_sequence) @error
+  (#any-of? @error "\\u000c" "\\u000C" "\\014" "\\14")
+  (#set! severity "error")
+  (#set! name "raw-special-escape")
+  (#set! title "Special escape sequence in octal/hex form")
+  (#set! label "Raw escape used here")
+  (#set! help "Replace the raw escape with \\f")
+  (#set! fix "\\f"))
+
+; special escape sequences encoded as octal/hex
+; https://google.github.io/styleguide/javaguide.html#s2.3.2-special-escape-sequences
+((escape_sequence) @error
+  (#any-of? @error "\\u000d" "\\u000D" "\\015" "\\15")
+  (#set! severity "error")
+  (#set! name "raw-special-escape")
+  (#set! title "Special escape sequence in octal/hex form")
+  (#set! label "Raw escape used here")
+  (#set! help "Replace the raw escape with \\r")
+  (#set! fix "\\r"))
 
 ; wildcard imports
 ; https://google.github.io/styleguide/javaguide.html#s3.3.1-wildcard-imports
 ((import_declaration
   (asterisk) @error)
-  (#set! severity "warning")
+  (#set! severity "error")
   (#set! name "wildcard-import")
   (#set! title "Do not use wildcard imports")
   (#set! label "Wildcard used here")
