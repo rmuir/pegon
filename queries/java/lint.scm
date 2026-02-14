2e156adcf0b7ace5b01c13229dc89e4183ae95ee
Robert Muir <rmuir@apache.org>
2026-02-13 23:03:07 -0500

    fix: cleanup

--- queries/java/lint.scm
+++ queries/java/lint.scm
@@ -4,10 +4,9 @@
   (MISSING)
 ] @error
   (#set! name "parse-error")
-  (#set! severity "warning")
   (#set! title "Parse Error")
-  (#set! label "Parse error here")
-  (#set! note "Correct the invalid Java syntax"))
+  (#set! note "Correct the invalid Java syntax")
+  (#set! severity "hint"))
 
 ; https://google.github.io/styleguide/javaguide.html#s2.3.1-whitespace-characters
 ([
@@ -16,121 +15,110 @@
   (multiline_string_fragment)
 ] @error
   (#match? @error "[\\s&&[^\\u0020\n]]")
-  (#set! severity "error")
-  (#set! name "illegal-whitespace")
-  (#set! title "Illegal whitespace character")
-  (#set! label "Whitespace used here")
-  (#set! note "Escape the character: only 0x20 may appear in literals"))
+  (#set! name "unescaped-literal-space")
+  (#set! title "Special whitespace in literal")
+  (#set! note "Escape the special whitespace: only 0x20 may appear in literals")
+  (#set! severity "error"))
 
 ; special escape sequences encoded as octal/hex
 ; https://google.github.io/styleguide/javaguide.html#s2.3.2-special-escape-sequences
 ((escape_sequence) @error
   (#any-of? @error "\\u0008" "\\010" "\\10")
-  (#set! severity "error")
-  (#set! name "raw-special-escape")
-  (#set! title "Special escape sequence in octal/hex form")
-  (#set! label "Raw escape used here")
-  (#set! note "Replace the raw escape with the special escape \\b"))
+  (#set! name "wrong-escape")
+  (#set! title "Backspace escaped incorrectly")
+  (#set! note "Replace the octal/hex escape with the special escape \\b")
+  (#set! severity "error"))
 
 ; special escape sequences encoded as octal/hex
 ; https://google.github.io/styleguide/javaguide.html#s2.3.2-special-escape-sequences
 ((escape_sequence) @error
   (#any-of? @error "\\u0009" "\\011" "\\11")
-  (#set! severity "error")
-  (#set! name "raw-special-escape")
-  (#set! title "Special escape sequence in octal/hex form")
-  (#set! label "Raw escape used here")
-  (#set! note "Replace the raw escape with the special escape \\t"))
+  (#set! name "wrong-escape")
+  (#set! title "Tab escaped incorrectly")
+  (#set! note "Replace the octal/hex escape with the special escape \\t")
+  (#set! severity "error"))
 
 ; special escape sequences encoded as octal/hex
 ; https://google.github.io/styleguide/javaguide.html#s2.3.2-special-escape-sequences
 ((escape_sequence) @error
   (#any-of? @error "\\u000a" "\\u000A" "\\012" "\\12")
-  (#set! severity "error")
-  (#set! name "raw-special-escape")
-  (#set! title "Special escape sequence in octal/hex form")
-  (#set! label "Raw escape used here")
-  (#set! note "Replace the raw escape with the special escape \\n"))
+  (#set! name "wrong-escape")
+  (#set! title "Newline escaped incorrectly")
+  (#set! note "Replace the octal/hex escape with the special escape \\n")
+  (#set! severity "error"))
 
 ; special escape sequences encoded as octal/hex
 ; https://google.github.io/styleguide/javaguide.html#s2.3.2-special-escape-sequences
 ((escape_sequence) @error
   (#any-of? @error "\\u000c" "\\u000C" "\\014" "\\14")
-  (#set! severity "error")
-  (#set! name "raw-special-escape")
-  (#set! title "Special escape sequence in octal/hex form")
-  (#set! label "Raw escape used here")
-  (#set! note "Replace the raw escape with the special escape \\f"))
+  (#set! name "wrong-escape")
+  (#set! title "Form feed escaped incorrectly")
+  (#set! note "Replace the octal/hex escape with the special escape \\f")
+  (#set! severity "error"))
 
 ; special escape sequences encoded as octal/hex
 ; https://google.github.io/styleguide/javaguide.html#s2.3.2-special-escape-sequences
 ((escape_sequence) @error
   (#any-of? @error "\\u000d" "\\u000D" "\\015" "\\15")
-  (#set! severity "error")
-  (#set! name "raw-special-escape")
-  (#set! title "Special escape sequence in octal/hex form")
-  (#set! label "Raw escape used here")
-  (#set! note "Replace the raw escape with the special escape \\r"))
+  (#set! name "wrong-escape")
+  (#set! title "Carriage return escaped incorrectly")
+  (#set! note "Replace the octal/hex escape with the special escape \\r")
+  (#set! severity "error"))
 
 ; special escape sequences encoded as octal/hex
 ; https://google.github.io/styleguide/javaguide.html#s2.3.2-special-escape-sequences
 ((escape_sequence) @error
   (#any-of? @error "\\u0022" "\\042" "\\42")
-  (#set! severity "error")
-  (#set! name "raw-special-escape")
-  (#set! title "Special escape sequence in octal/hex form")
-  (#set! label "Raw escape used here")
-  (#set! note "Replace the raw escape with the special escape \\\""))
+  (#set! name "wrong-escape")
+  (#set! title "Double quote escaped incorrectly")
+  (#set! note "Replace the octal/hex escape with the special escape \\\"")
+  (#set! severity "error"))
 
 ; special escape sequences encoded as octal/hex
 ; https://google.github.io/styleguide/javaguide.html#s2.3.2-special-escape-sequences
 ((escape_sequence) @error
   (#any-of? @error "\\u0027" "\\047" "\\47")
-  (#set! severity "error")
-  (#set! name "raw-special-escape")
-  (#set! title "Special escape sequence in octal/hex form")
-  (#set! label "Raw escape used here")
-  (#set! note "Replace the raw escape with the special escape \\'"))
+  (#set! name "wrong-escape")
+  (#set! title "Single quote escaped incorrectly")
+  (#set! note "Replace the octal/hex escape with the special escape \\'")
+  (#set! severity "error"))
 
 ; special escape sequences encoded as octal/hex
 ; https://google.github.io/styleguide/javaguide.html#s2.3.2-special-escape-sequences
 ((escape_sequence) @error
   (#any-of? @error "\\u005c" "\\u005C" "\\134")
-  (#set! severity "error")
-  (#set! name "raw-special-escape")
-  (#set! title "Special escape sequence in octal/hex form")
-  (#set! label "Raw escape used here")
-  (#set! note "Replace the raw escape with the special escape \\\\"))
+  (#set! name "wrong-escape")
+  (#set! title "Backslash escaped incorrectly")
+  (#set! note "Replace the raw escape with the special escape \\\\")
+  (#set! severity "error"))
 
 ; line-wrapped package declaration
 ; https://google.github.io/styleguide/javaguide.html#s3.2-package-declaration
 ((package_declaration) @error
   (#match? @error "\n")
-  (#set! severity "error")
   (#set! name "wrapped-package")
-  (#set! title "Do not line-wrap package declarations")
-  (#set! label "package declared here")
-  (#set! note "Remove newlines from the package statement"))
+  (#set! title "Line-wrapped package declaration")
+  (#set! note "Remove newlines from the package statement")
+  (#set! severity "error"))
 
 ; wildcard imports
 ; https://google.github.io/styleguide/javaguide.html#s3.3.1-wildcard-imports
 ((import_declaration
+  (_) ; TODO: thing being imported
   (asterisk) @error)
-  (#set! severity "error")
   (#set! name "wildcard-import")
   (#set! title "Wildcard import")
-  (#set! label "Wildcard used here")
-  (#set! note "Replace the wildcard import with standard import(s)"))
+  (#set! note "Replace the wildcard import with standard import(s)")
+  (#set! severity "error"))
 
 ; line-wrapped imports
 ; https://google.github.io/styleguide/javaguide.html#s3.3.2-import-line-wrapping
 ((import_declaration) @error
   (#match? @error "\n")
-  (#set! severity "error")
-  (#set! name "wrapped-import")
+  (#set! name "line-wrapped-import")
   (#set! title "Line-wrapped import")
-  (#set! label "import declared here")
-  (#set! note "Remove newlines from the import statement"))
+  (#set! note "Remove newlines from the import statement")
+  (#set! severity "error"))
 
 ; multiple top-level classes in the same file
 ; https://google.github.io/styleguide/javaguide.html#s3.4.1-one-top-level-class
@@ -139,88 +127,78 @@
   (class_declaration
     name: (identifier) @error)
   (#set! name "multiple-classes")
-  (#set! severity "error")
   (#set! title "Multiple top-level classes")
-  (#set! label "Additional top-level class defined here")
-  (#set! note "Move top-level classes into their own files: only one per file"))
+  (#set! note "Move top-level classes into their own files: only one per file")
+  (#set! severity "error"))
 
 ; integer literal with lowercase 'l'
 ; https://google.github.io/styleguide/javaguide.html#s4.8.8-numeric-literals
 ((decimal_integer_literal) @error
   (#match? @error "l$")
-  (#set! name "lower-long-literal")
-  (#set! severity "error")
-  (#set! title "Lowercase suffix used for long literal")
-  (#set! label "literal used here")
-  (#set! note "Change to an uppercase L suffix to improve legibility"))
+  (#set! name "lowercase-long-literal")
+  (#set! title "Lowercase long integer literal")
+  (#set! note "Replace with uppercase L suffix to improve legibility")
+  (#set! severity "error"))
 
 ; identifier containing illegal character
 ; https://google.github.io/styleguide/javaguide.html#s5.1-identifier-names
 ((identifier) @error
   (#match? @error "[^a-zA-Z0-9_]")
-  (#set! severity "error")
-  (#set! name "invalid-identifier-name")
-  (#set! title "Illegal characters used in identifier")
-  (#set! label "identifier here")
-  (#set! note "Change to use only ASCII letters, digits, and underscores"))
+  (#set! name "invalid-identifier")
+  (#set! title "Invalid identifier")
+  (#set! note "Rename using only ASCII letters, digits, and underscores")
+  (#set! severity "error"))
 
 ; package names should be lowercase and digits only
 ; https://google.github.io/styleguide/javaguide.html#s5.2.1-package-names
 ((package_declaration
   (identifier) @error)
   (#match? @error "[^a-z0-9]")
-  (#set! severity "error")
   (#set! name "invalid-package-name")
-  (#set! title "Illegal characters used in package name")
-  (#set! label "package declared here")
-  (#set! note "Change package name to use lowercase and digits"))
+  (#set! title "Invalid package name")
+  (#set! note "Rename package using only lowercase and digits")
+  (#set! severity "error"))
 
 ; module names should be lowercase and digits only
 ; https://google.github.io/styleguide/javaguide.html#s5.2.1-package-names
 ((module_declaration
   (identifier) @error)
   (#match? @error "[^a-z0-9]")
-  (#set! severity "error")
   (#set! name "invalid-module-name")
-  (#set! title "Illegal characters used in module name")
-  (#set! label "module declared here")
-  (#set! note "Change module name to use lowercase and digits"))
+  (#set! title "Invalid module name")
+  (#set! note "Rename module using only lowercase and digits")
+  (#set! severity "error"))
 
 ; class names should be UpperCamelCase
 ; https://google.github.io/styleguide/javaguide.html#s5.2.2-class-names
 ((class_declaration
   name: (identifier) @error)
   (#match? @error "^[a-z]")
-  (#set! severity "error")
   (#set! name "invalid-class-name")
-  (#set! title "Lowercase class name")
-  (#set! label "class declared here")
-  (#set! note "Change class name to use UpperCamelCase"))
+  (#set! title "Invalid class name")
+  (#set! note "Rename class using UpperCamelCase")
+  (#set! severity "error"))
 
 ; parameters should be lowerCamelCase
 ; https://google.github.io/styleguide/javaguide.html#s5.2.6-parameter-names
-(formal_parameters
-  ((formal_parameter
-    name: (identifier) @error)
-    (#match? @error "^[A-Z]")
-    (#set! severity "error")
-    (#set! name "invalid-param-name")
-    (#set! title "Uppercase parameter name")
-    (#set! label "parameter declared here")
-    (#set! note "Change parameter name to use lowerCamelCase"))) @visible
+((formal_parameter
+  name: (identifier) @error)
+  (#match? @error "^[A-Z]")
+  (#set! name "invalid-param-name")
+  (#set! title "Invalid parameter name")
+  (#set! note "Rename parameter using lowerCamelCase")
+  (#set! severity "error")) @visible
 
 ; spread parameter should be lowerCamelCase
 ; https://google.github.io/styleguide/javaguide.html#s5.2.6-parameter-names
-(formal_parameters
-  (spread_parameter
-    (variable_declarator
-      name: (identifier) @error)
-    (#match? @error "^[A-Z]")
-    (#set! severity "error")
-    (#set! name "invalid-param-name")
-    (#set! title "Uppercase parameter name")
-    (#set! label "parameter declared here")
-    (#set! note "Change parameter name to use lowerCamelCase"))) @visible
+(spread_parameter
+  (variable_declarator
+    name: (identifier) @error)
+  (#match? @error "^[A-Z]")
+  (#set! name "invalid-param-name")
+  (#set! title "Invalid vararg parameter name")
+  (#set! note "Rename vararg parameter using lowerCamelCase")
+  (#set! severity "error")) @visible
 
 ; local variables should be lowerCamelCase
 ; https://google.github.io/styleguide/javaguide.html#s5.2.7-local-variable-names
@@ -228,22 +206,20 @@
   declarator: (variable_declarator
     name: (identifier) @error)
   (#match? @error "^[A-Z]")
-  (#set! severity "error")
   (#set! name "invalid-local-name")
-  (#set! title "Uppercase local variable name")
-  (#set! label "variable declared here")
-  (#set! note "Change variable name to use lowerCamelCase")) @visible
+  (#set! title "Invalid local variable name")
+  (#set! note "Rename local variable using lowerCamelCase")
+  (#set! severity "error")) @visible
 
 ; type variables should be UpperCamelCase
 ; https://google.github.io/styleguide/javaguide.html#s5.2.8-type-variable-names
 ((type_parameter
   (type_identifier) @error)
   (#match? @error "^[a-z]")
-  (#set! severity "error")
   (#set! name "invalid-type-name")
-  (#set! title "Lowercase type name")
-  (#set! label "type variable declared here")
-  (#set! note "Change type name to use UpperCamelCase"))
+  (#set! title "Invalid type name")
+  (#set! note "Rename type using UpperCamelCase")
+  (#set! severity "error")) @visible
 
 ; Caught exceptions: not ignored
 ; https://google.github.io/styleguide/javaguide.html#s6.2-caught-exceptions
@@ -256,23 +232,22 @@
   (#not-eq? @error "_")
   ; no real content at all
   (#not-match? @_block "[a-zA-Z0-9_]")
-  (#set! severity "error")
-  (#set! name "ignored-caught-exception")
-  (#set! title "Caught exception ignored")
-  (#set! label "exception being ignored")
-  (#set! note "Handle the exception, add a comment, or indicate via unnamed variable _")) @visible
+  (#set! name "unhandled-exception")
+  (#set! title "Unhandled caught exception")
+  (#set! note "Handle the exception, add a comment, or indicate via unnamed variable _")
+  (#set! severity "error")) @visible ; body is small (empty)
 
 ; Finalizers: not used
 ; https://google.github.io/styleguide/javaguide.html#s6.4-finalizers
 ((method_declaration
-  type: (void_type)
+  type: (void_type) @visible
+  ; body could be large
   name: (identifier) @error
   parameters: (formal_parameters) @_params)
   (#eq? @error "finalize")
   ; only parentheses
   (#match? @_params "^[\\s]*[(][\\s]*[)][\\s]*$")
-  (#set! severity "error")
   (#set! name "finalizer-used")
-  (#set! title "Do not override Object.finalize")
-  (#set! label "override here")
-  (#set! note "Migrate to other resource management such as try-with-resources or cleaners"))
+  (#set! title "Finalizer used")
+  (#set! note "Migrate to other resource management such as try-with-resources or cleaners")
+  (#set! severity "error"))
