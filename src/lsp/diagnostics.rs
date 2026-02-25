52345b478473e5d30cb1b15f4ee0ab36ffe0cd71
Robert Muir <rmuir@apache.org>
2026-02-25 09:47:58 -0500

    feat: simplify

--- src/lsp/diagnostics.rs
+++ src/lsp/diagnostics.rs
@@ -1,4 +1,4 @@
-use anyhow::{Context, Result};
+use anyhow::{Context, Error, Result};
 use line_index::LineIndex;
 use lsp_server::Message;
 use lsp_types::{
@@ -37,8 +37,12 @@ pub fn pull_diagnostics(
     docs: &FxHashMap<String, OpenDocument>,
     previous_result_id: Option<String>,
 ) -> Result<DocumentDiagnosticReportKind> {
-    docs.get(&uri.to_string()).context("unknown doc")?;
-    let (result_id, items) = diagnostics(client, uri, docs)?;
+    let doc = docs.get(&uri.to_string()).context("unknown doc")?;
+    let line_index = LineIndex::new(&doc.text);
+    let bytes = doc.text.as_bytes();
+    let results = lint(&doc.tree, bytes)?;
+    let result_id = hash_items(&results);
+
     if let Some(previous_id) = previous_result_id
         && previous_id == result_id
     {
@@ -48,7 +52,7 @@ pub fn pull_diagnostics(
     } else {
         Ok(DocumentDiagnosticReportKind::Full(
             FullDocumentDiagnosticReport {
-                items,
+                items: encode(client, uri, &line_index, &results)?,
                 result_id: Some(result_id),
             },
         ))
@@ -62,14 +66,17 @@ pub fn push_diagnostics(
     docs: &FxHashMap<String, OpenDocument>,
 ) -> Result<()> {
     let doc = docs.get(&uri.to_string()).context("unknown doc")?;
-    let (_, items) = diagnostics(client, uri, docs)?;
+    let line_index = LineIndex::new(&doc.text);
+    let bytes = doc.text.as_bytes();
+    let results = lint(&doc.tree, bytes)?;
+
     client
         .connection
         .sender
         .send(Message::Notification(lsp_server::Notification::new(
             PublishDiagnostics::METHOD.to_owned(),
             PublishDiagnosticsParams {
-                diagnostics: items,
+                diagnostics: encode(client, uri, &line_index, &results)?,
                 uri: uri.clone(),
                 version: if client.version_support() {
                     Some(doc.version)
@@ -87,90 +94,85 @@ fn hash_items(items: &Vec<Lint>) -> String {
     format!("{:016x}", hasher.finish())
 }
 
-/// return diagnostics
-fn diagnostics(
+/// encode diagnostics into LSP structure
+fn encode(
     client: &Client,
     uri: &Uri,
-    docs: &FxHashMap<String, OpenDocument>,
-) -> Result<(String, Vec<Diagnostic>)> {
-    let doc = docs
-        .get(&uri.to_string())
-        .context("document should exist")?;
-
-    let line_index = LineIndex::new(&doc.text);
-    let bytes = doc.text.as_bytes();
-
-    let results = lint(&doc.tree, bytes)?;
-    let result_id = hash_items(&results);
-
-    Ok((
-        result_id,
-        results
-            .iter()
-            .filter_map(|diagnostic| {
-                let rule = rule(diagnostic.rule_id);
-                let start = client.encode_position(diagnostic.range.start, &line_index)?;
-                let end = client.encode_position(diagnostic.range.end, &line_index)?;
-                let lsp_severity = rule.severity.into();
-                // all the context ranges are related information
-                let mut related_information = diagnostic
-                    .context
-                    .iter()
-                    .filter_map(|context| {
-                        let related_start = client.encode_position(context.start, &line_index)?;
-                        let related_end = client.encode_position(context.end, &line_index)?;
-                        let related = DiagnosticRelatedInformation {
-                            location: Location {
-                                uri: uri.clone(),
-                                range: Range::new(related_start, related_end),
-                            },
-                            message: rule.context_label.clone().unwrap_or_default(),
-                        };
-                        Some(related)
-                    })
-                    .collect::<Vec<_>>();
-                // optional label maps to related information at node's position
-                if let Some(label) = &diagnostic.label {
-                    related_information.push(DiagnosticRelatedInformation {
+    line_index: &LineIndex,
+    results: &[Lint],
+) -> Result<Vec<Diagnostic>> {
+    results
+        .iter()
+        .map(|diagnostic| {
+            let rule = rule(diagnostic.rule_id);
+            let start = client
+                .encode_position(diagnostic.range.start, line_index)
+                .context("invalid start offset")?;
+            let end = client
+                .encode_position(diagnostic.range.end, line_index)
+                .context("invalid end offset")?;
+            let lsp_severity = rule.severity.into();
+            // all the context ranges are related information
+            let mut related_information = diagnostic
+                .context
+                .iter()
+                .map(|context| {
+                    let related_start = client
+                        .encode_position(context.start, line_index)
+                        .context("invalid context start offset")?;
+                    let related_end = client
+                        .encode_position(context.end, line_index)
+                        .context("invalid context end offset")?;
+                    Ok(DiagnosticRelatedInformation {
                         location: Location {
                             uri: uri.clone(),
-                            range: Range::new(start, end),
+                            range: Range::new(related_start, related_end),
                         },
-                        message: label.clone(),
-                    });
-                }
-                // help text maps to related information at node's position
+                        message: rule.context_label.clone().unwrap_or_default(),
+                    })
+                })
+                .collect::<Result<Vec<_>, Error>>()?;
+            // optional label maps to related information at node's position
+            if let Some(label) = &diagnostic.label {
                 related_information.push(DiagnosticRelatedInformation {
                     location: Location {
                         uri: uri.clone(),
                         range: Range::new(start, end),
                     },
-                    message: diagnostic.help.clone(),
+                    message: label.clone(),
                 });
-                Some(Diagnostic {
+            }
+            // help text maps to related information at node's position
+            related_information.push(DiagnosticRelatedInformation {
+                location: Location {
+                    uri: uri.clone(),
                     range: Range::new(start, end),
-                    severity: Some(lsp_severity),
-                    code: Some(NumberOrString::String(rule.name.clone())),
-                    code_description: if client.code_description_support() {
-                        Some(CodeDescription {
-                            href: Uri::from_str(&rule.url).expect("rule url should exist"),
-                        })
-                    } else {
-                        None
-                    },
-                    source: Some("pegon".to_string()),
-                    message: diagnostic.title.clone(),
-                    related_information: if client.related_information_support() {
-                        Some(related_information)
-                    } else {
-                        None
-                    },
-                    tags: None,
-                    data: None,
-                })
+                },
+                message: diagnostic.help.clone(),
+            });
+            Ok(Diagnostic {
+                range: Range::new(start, end),
+                severity: Some(lsp_severity),
+                code: Some(NumberOrString::String(rule.name.clone())),
+                code_description: if client.code_description_support() {
+                    Some(CodeDescription {
+                        href: Uri::from_str(&rule.url).expect("rule url should exist"),
+                    })
+                } else {
+                    None
+                },
+                source: Some("pegon".to_string()),
+                message: diagnostic.title.clone(),
+                related_information: if client.related_information_support() {
+                    Some(related_information)
+                } else {
+                    None
+                },
+                tags: None,
+                data: None,
             })
-            .collect::<Vec<_>>(),
-    ))
+        })
+        .collect()
 }
 
 // for push clients, clear diagnostic space, e.g. on document close
