13c35c8475d1a7cb4716c74154bab555a21e0464
Robert Muir <rmuir@apache.org>
2026-03-07 14:59:40 -0500

    fix: manpages

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,3 +1,4 @@
+use clap::CommandFactory as _;
 use core::error::Error;
 use pegon::cli::Cli;
 use std::{env, fs};
@@ -6,14 +7,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     // put manpages in CWD/target/man
     let out_dir = env::current_dir()?.join("target").join("man");
     fs::create_dir_all(&out_dir)?;
-
-    // Build the clap::Command from the derive type.
-    let cmd = <Cli as clap::CommandFactory>::command();
-
-    // Generate a man page for the root and all subcommands recursively.
-    clap_mangen::generate_to(cmd, &out_dir)?;
-
+    clap_mangen::generate_to(Cli::command(), &out_dir)?;
     println!("Generated man pages to {}", out_dir.display());
-
     Ok(())
 }
