1d59b6839fa6cd8a4ccdbaacced600024695d966
Robert Muir <rmuir@apache.org>
2026-03-14 09:23:15 -0400

    fix: updates and cleanups

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -5,31 +5,7 @@ use pegon::cli::Cli;
 use std::{env, fs};
 
 fn main() -> Result<(), Box<dyn Error>> {
-    help()?;
-    manpages()?;
     completions()?;
-    Ok(())
-}
-
-/// output help to `$CWD/docs/README.md`
-fn help() -> Result<(), Box<dyn Error>> {
-    let mut command = Cli::command().flatten_help(true).disable_colored_help(true);
-    let help = format!("```text\n{}\n```\n", command.render_long_help());
-    let out_dir = env::current_dir()?.join("docs");
-    fs::write(out_dir.join("README.md"), help)?;
-    println!("Generated help to {}", out_dir.display());
-    Ok(())
-}
-
-/// output manual pages to `$CWD/docs/man`
-fn manpages() -> Result<(), Box<dyn Error>> {
-    let out_dir = env::current_dir()?.join("docs").join("man");
-    if out_dir.exists() {
-        fs::remove_dir_all(&out_dir)?;
-    }
-    fs::create_dir_all(&out_dir)?;
-    clap_mangen::generate_to(Cli::command(), &out_dir)?;
-    println!("Generated man pages to {}", out_dir.display());
     Ok(())
 }
 
