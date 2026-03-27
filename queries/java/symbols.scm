2fb754d37408d07ae53f46c890bd13c05bf4150f
Robert Muir <rmuir@apache.org>
2026-03-26 21:13:28 -0400

    fix: rename

--- queries/java/symbols.scm
+++ queries/java/symbols.scm
@@ -1,51 +1,52 @@
 ; classes
 ; kind=11 (Interface) ?
 (annotation_type_declaration
-  name: (identifier) @name) @definition.annotation
+  name: (identifier) @name) @range
 
 ; kind=5 (Class)
 (class_declaration
-  name: (identifier) @name) @definition.class
+  name: (identifier) @name
+  type_parameters: (type_parameters)? @detail) @range
 
 ; kind=10 (Enum)
 (enum_declaration
-  name: (identifier) @name) @definition.enum
+  name: (identifier) @name) @range
 
 ; kind=11 (Interface)
 (interface_declaration
-  name: (identifier) @name) @definition.interface
+  name: (identifier) @name) @range
 
 ; kind=23 (Struct fallback to Class)
 (record_declaration
-  name: (identifier) @name) @definition.record
+  name: (identifier) @name) @range
 
 ; "members"
 ; kind=6 (Method)
 (annotation_type_element_declaration
-  name: (identifier) @name) @definition.annotationElement ; field?
+  name: (identifier) @name) @range
 
 ; kind=9 (Constructor)
 (compact_constructor_declaration
-  name: (identifier) @name) @definition.constructor
+  name: (identifier) @name) @range
 
 ; kind=14 (Constant)
 (constant_declaration
   declarator: (variable_declarator
-    name: (identifier) @name)) @definition.field
+    name: (identifier) @name)) @range
 
 ; kind=9 (Constructor)
 (constructor_declaration
-  name: (identifier) @name) @definition.constructor
+  name: (identifier) @name) @range
 
 ; kind=22 (EnumMember fallback to Field)
 (enum_constant
-  name: (identifier) @name) @definition.enum_constant
+  name: (identifier) @name) @range
 
 ; kind=8 (Field)
 (field_declaration
   declarator: (variable_declarator
-    name: (identifier) @name)) @definition.field
+    name: (identifier) @name)) @range
 
 ; kind=6 (Method)
 (method_declaration
-  name: (identifier) @name) @definition.method
+  name: (identifier) @name) @range
