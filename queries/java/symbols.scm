7a9071a522e5aa10e9d2dbd76599246a26ad5bb3
Robert Muir <rmuir@apache.org>
2026-03-26 21:36:01 -0400

    feat: details now too

--- queries/java/symbols.scm
+++ queries/java/symbols.scm
@@ -14,15 +14,18 @@
 
 ; kind=11 (Interface)
 (interface_declaration
-  name: (identifier) @name) @range
+  name: (identifier) @name
+  type_parameters: (type_parameters)? @detail) @range
 
 ; kind=23 (Struct fallback to Class)
 (record_declaration
-  name: (identifier) @name) @range
+  name: (identifier) @name
+  type_parameters: (type_parameters)? @detail) @range
 
 ; "members"
 ; kind=6 (Method)
 (annotation_type_element_declaration
+  type: (_) @detail
   name: (identifier) @name) @range
 
 ; kind=9 (Constructor)
@@ -31,12 +34,14 @@
 
 ; kind=14 (Constant)
 (constant_declaration
+  type: (_) @detail
   declarator: (variable_declarator
     name: (identifier) @name)) @range
 
 ; kind=9 (Constructor)
 (constructor_declaration
-  name: (identifier) @name) @range
+  name: (identifier) @name
+  parameters: (formal_parameters) @detail) @range
 
 ; kind=22 (EnumMember fallback to Field)
 (enum_constant
@@ -44,9 +49,16 @@
 
 ; kind=8 (Field)
 (field_declaration
+  type: (_) @detail
   declarator: (variable_declarator
     name: (identifier) @name)) @range
 
 ; kind=6 (Method)
 (method_declaration
-  name: (identifier) @name) @range
+  name: (identifier) @name
+  parameters: (formal_parameters) @detail) @range
+
+; anonymous class: not represented here but needed for proper hierarchy
+(object_creation_expression
+  type: (type_identifier) @name
+  (class_body) @definition.class) ; anonymous
