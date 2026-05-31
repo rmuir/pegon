583a32016147dbeb0dcdd89653649333e055621f
Robert Muir <rmuir@apache.org>
2026-05-31 17:26:01 -0400

    feat: more hover queries

--- queries/java/hover.scm
+++ queries/java/hover.scm
@@ -78,8 +78,47 @@
 ; abstract keyword on a class
 ((class_declaration
   (modifiers
-    (modifier) @range))
-  (#eq? @range "abstract")
+    (modifier
+      "abstract") @range))
   (#set! hover.description "This class isn't concrete: only subclasses can be instantiated.")
   (#set! hover.kind "abstract class modifier")
   (#set! hover.spec "jls-8.html#jls-8.1.1.1"))
+
+; abstract keyword on a method
+((method_declaration
+  (modifiers
+    (modifier
+      "abstract") @range))
+  (#set! hover.description "This method isn't concrete: a subclass must implement it.")
+  (#set! hover.kind "abstract method modifier")
+  (#set! hover.spec "jls-8.html#jls-8.4.3.1"))
+
+; abstract keyword on a method
+; TODO: add a diagnostic?
+((interface_declaration
+  (modifiers
+    (modifier
+      "abstract") @range))
+  (#set! hover.description "All interfaces are abstract: remove this keyword.")
+  (#set! hover.kind "abstract interface modifier")
+  (#set! hover.spec "jls-9.html#jls-9.1.1.1"))
+
+; assertion keyword
+((assert_statement
+  "assert" @range)
+  (#set! hover.description "Raises `AssertionError` on failure if assertions are enabled.")
+  (#set! hover.kind "assert statement")
+  (#set! hover.spec "jls-14.html#jls-14.10"))
+
+; boolean keyword
+((boolean_type) @range
+  (#set! hover.description "Boolean type: `true` or `false`")
+  (#set! hover.kind "boolean type")
+  (#set! hover.spec "jls-4.html#jls-4.2.5"))
+
+; boolean used as a field
+((field_declaration
+  type: (boolean_type) @range)
+  (#set! hover.description "Boolean type: `true` or `false`. Probably uses a byte of space here.")
+  (#set! hover.kind "boolean field")
+  (#set! hover.spec "jls-4.html#jls-4.2.5"))
