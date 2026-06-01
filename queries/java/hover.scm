75a1e61539d5641075c8ce49fa837e7ee6dc59ab
Robert Muir <rmuir@apache.org>
2026-06-01 07:27:51 -0400

    feat: more hover

--- queries/java/hover.scm
+++ queries/java/hover.scm
@@ -1,18 +1,12 @@
 ; always a keyword
 ([
-  "abstract"
-  "assert"
-  (boolean_type)
   "break"
-  (integral_type)
   "case"
   "catch"
   "class"
   "continue"
   "default"
   "do"
-  (floating_point_type)
-  "double"
   "else"
   "enum"
   "extends"
@@ -93,7 +87,7 @@
   (#set! hover.kind "abstract method modifier")
   (#set! hover.spec "jls-8.html#jls-8.4.3.1"))
 
-; abstract keyword on a method
+; abstract keyword on an interface
 ; TODO: add a diagnostic?
 ((interface_declaration
   (modifiers
@@ -104,8 +98,7 @@
   (#set! hover.spec "jls-9.html#jls-9.1.1.1"))
 
 ; assertion keyword
-((assert_statement
-  "assert" @range)
+("assert" @range
   (#set! hover.description "Raises `AssertionError` on failure if assertions are enabled.")
   (#set! hover.kind "assert statement")
   (#set! hover.spec "jls-14.html#jls-14.10"))
@@ -122,3 +115,46 @@
   (#set! hover.description "Boolean type: `true` or `false`. Probably uses a byte of space here.")
   (#set! hover.kind "boolean field")
   (#set! hover.spec "jls-4.html#jls-4.2.5"))
+
+; byte
+("byte" @range
+  (#set! hover.description "8-bit signed integer: -128 .. 127.")
+  (#set! hover.kind "byte type")
+  (#set! hover.spec "jls-4.html#jls-4.2.1"))
+
+; char
+("char" @range
+  (#set! hover.description "16-bit unsigned integer: 0 .. 65,535 (\\u0000-\\uFFFF).")
+  (#set! hover.kind "char type")
+  (#set! hover.spec "jls-4.html#jls-4.2.1"))
+
+; short
+("short" @range
+  (#set! hover.description "16-bit signed integer: -32,768 .. 32,767.")
+  (#set! hover.kind "short type")
+  (#set! hover.spec "jls-4.html#jls-4.2.1"))
+
+; int
+("int" @range
+  (#set! hover.description "32-bit signed integer: -2,147,483,648 .. 2,147,483,647.")
+  (#set! hover.kind "integer type")
+  (#set! hover.spec "jls-4.html#jls-4.2.1"))
+
+; long
+("long" @range
+  (#set! hover.description
+    "64-bit signed integer: -9,223,372,036,854,775,808 .. 9,223,372,036,854,775,807.")
+  (#set! hover.kind "long integer type")
+  (#set! hover.spec "jls-4.html#jls-4.2.1"))
+
+; float
+("float" @range
+  (#set! hover.description "32-bit IEEE binary32 float: 8-bit exponent.")
+  (#set! hover.kind "single-precision floating-point type")
+  (#set! hover.spec "jls-4.html#jls-4.2.3"))
+
+; float
+("double" @range
+  (#set! hover.description "64-bit IEEE binary64 float: 11-bit exponent.")
+  (#set! hover.kind "double-precision floating-point type")
+  (#set! hover.spec "jls-4.html#jls-4.2.3"))
