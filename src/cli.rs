4ce779ede5785424e6207ccf1e95accc20172c84
Robert Muir <rmuir@apache.org>
2026-06-07 22:06:50 -0400

    refactor: more modularization

--- src/cli.rs
+++ src/cli.rs
@@ -1,77 +1,15 @@
 //! Command line interface
 
 use core::net::Ipv4Addr;
-use std::path::PathBuf;
 
 mod diagnostics;
+mod parser;
 
 use anyhow::Error;
-use clap::{
-    Parser, Subcommand, ValueEnum,
-    builder::styling::{AnsiColor, Styles},
-};
-
+use clap::Parser as _;
 use lsp_server::Connection;
 
-/// Fast linter for the Google Java Style.
-///
-/// More information
-#[derive(Parser)]
-#[command(author, version)]
-#[command(arg_required_else_help = true)]
-#[command(disable_help_subcommand = true)]
-#[command(styles = CLI_STYLES)]
-pub struct Cli {
-    #[command(subcommand)]
-    pub command: Commands,
-}
-
-#[derive(Subcommand)]
-pub enum Commands {
-    /// Run pegon on the given files or directories.
-    ///
-    /// More information
-    Check {
-        /// List of files or directories to check
-        ///
-        /// Use `-` for standard input. [default: CWD]
-        files: Vec<PathBuf>,
-
-        /// Apply fixes to resolve lint violations.
-        #[arg(long)]
-        fix: bool,
-
-        /// Diagnostic error format
-        #[arg(long, value_enum, id = "FMT", default_value_t = OutputFormat::Full)]
-        output_format: OutputFormat,
-    },
-
-    /// Run the language server
-    #[group(required = false, multiple = false)]
-    Server {
-        /// Use standard I/O streams (default)
-        #[arg(long)]
-        stdio: bool,
-
-        /// Listen on loopback TCP socket
-        #[arg(long, id = "PORT")]
-        socket: Option<u16>,
-    },
-}
-
-#[derive(ValueEnum, Clone, PartialEq, Eq)]
-pub enum OutputFormat {
-    /// Cargo-style format
-    Full,
-    /// Grep-style format
-    Concise,
-}
-
-const CLI_STYLES: Styles = Styles::styled()
-    .header(AnsiColor::Green.on_default().bold())
-    .usage(AnsiColor::Green.on_default().bold())
-    .literal(AnsiColor::Blue.on_default().bold())
-    .placeholder(AnsiColor::Cyan.on_default());
+use crate::cli::parser::{Cli, Commands, OutputFormat};
 
 /// CLI entrypoint
 ///
---
+++ src/cli/parser.rs
@@ -0,0 +1,68 @@
+//! Command line parser
+
+use std::path::PathBuf;
+
+use clap::{
+    Parser, Subcommand, ValueEnum,
+    builder::styling::{AnsiColor, Styles},
+};
+
+/// Fast linter for the Google Java Style.
+///
+/// More information
+#[derive(Parser)]
+#[command(author, version)]
+#[command(arg_required_else_help = true)]
+#[command(disable_help_subcommand = true)]
+#[command(styles = CLI_STYLES)]
+pub struct Cli {
+    #[command(subcommand)]
+    pub command: Commands,
+}
+
+#[derive(Subcommand)]
+pub enum Commands {
+    /// Run pegon on the given files or directories.
+    ///
+    /// More information
+    Check {
+        /// List of files or directories to check
+        ///
+        /// Use `-` for standard input. [default: CWD]
+        files: Vec<PathBuf>,
+
+        /// Apply fixes to resolve lint violations.
+        #[arg(long)]
+        fix: bool,
+
+        /// Diagnostic error format
+        #[arg(long, value_enum, id = "FMT", default_value_t = OutputFormat::Full)]
+        output_format: OutputFormat,
+    },
+
+    /// Run the language server
+    #[group(required = false, multiple = false)]
+    Server {
+        /// Use standard I/O streams (default)
+        #[arg(long)]
+        stdio: bool,
+
+        /// Listen on loopback TCP socket
+        #[arg(long, id = "PORT")]
+        socket: Option<u16>,
+    },
+}
+
+#[derive(ValueEnum, Clone, PartialEq, Eq)]
+pub enum OutputFormat {
+    /// Cargo-style format
+    Full,
+    /// Grep-style format
+    Concise,
+}
+
+const CLI_STYLES: Styles = Styles::styled()
+    .header(AnsiColor::Green.on_default().bold())
+    .usage(AnsiColor::Green.on_default().bold())
+    .literal(AnsiColor::Blue.on_default().bold())
+    .placeholder(AnsiColor::Cyan.on_default());
