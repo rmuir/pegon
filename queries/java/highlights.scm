2ec2d28412e4f5f21d5b59282a9fc7668e67fe66
Robert Muir <rmuir@apache.org>
2026-06-20 18:48:49 -0400

    feat: highlight switch/case/default

--- queries/java/highlights.scm
+++ queries/java/highlights.scm
@@ -1,4 +1,24 @@
-; hover over "try" to highlight catch
+; highlight switch/case/default
+((switch_expression
+  "switch" @range @reference
+  body: (switch_block
+    [
+      (switch_block_statement_group
+        (switch_label
+          [
+            "case"
+            "default"
+          ] @range @reference))
+      (switch_rule
+        (switch_label
+          [
+            "case"
+            "default"
+          ] @range @reference))
+    ]*))
+  (#set! highlight.kind 2))
+
+; highlight try/catch/finally
 ((try_statement
   "try" @range @reference
   [
@@ -9,6 +29,7 @@
   ]*)
   (#set! highlight.kind 2))
 
+; highlight try/catch/finally
 ((try_with_resources_statement
   "try" @range @reference
   [
