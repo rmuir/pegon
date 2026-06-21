5fb5e366eb87aa5fd31f3df8eeb5fb4c0af47f0a
Robert Muir <rmuir@apache.org>
2026-06-20 20:39:53 -0400

    feat: mucho brace matching

--- queries/java/highlights.scm
+++ queries/java/highlights.scm
@@ -51,3 +51,193 @@
       "finally" @range @reference)
   ]*)
   (#set! highlight.kind 2))
+
+; do block start/end
+((do_statement
+  "do" @reference
+  body: (block
+    "{" @range @reference
+    "}" @range @reference))
+  (#set! highlight.kind 2))
+
+; synchronized block start/end
+((synchronized_statement
+  "synchronized" @reference
+  body: (block
+    "{" @range @reference
+    "}" @range @reference))
+  (#set! highlight.kind 2))
+
+; try block start/end
+((try_statement
+  "try" @reference
+  body: (block
+    "{" @range @reference
+    "}" @range @reference))
+  (#set! highlight.kind 2))
+
+; catch block start/end
+((catch_clause
+  "catch" @reference
+  body: (block
+    "{" @range @reference
+    "}" @range @reference))
+  (#set! highlight.kind 2))
+
+; finally block start/end
+((finally_clause
+  "finally" @reference
+  (block
+    "{" @range @reference
+    "}" @range @reference))
+  (#set! highlight.kind 2))
+
+; try-with-resources block start/end
+((try_with_resources_statement
+  "try" @reference
+  body: (block
+    "{" @range @reference
+    "}" @range @reference))
+  (#set! highlight.kind 2))
+
+; if block start/end
+((if_statement
+  "if" @reference
+  consequence: (block
+    "{" @range @reference
+    "}" @range @reference))
+  (#set! highlight.kind 2))
+
+; if block start/end
+((if_statement
+  "else" @reference
+  alternative: (block
+    "{" @range @reference
+    "}" @range @reference))
+  (#set! highlight.kind 2))
+
+; while block start/end
+((while_statement
+  "while" @reference
+  body: (block
+    "{" @range @reference
+    "}" @range @reference))
+  (#set! highlight.kind 2))
+
+; for block start/end
+((for_statement
+  "for" @reference
+  body: (block
+    "{" @range @reference
+    "}" @range @reference))
+  (#set! highlight.kind 2))
+
+; for block start/end
+((enhanced_for_statement
+  "for" @reference
+  body: (block
+    "{" @range @reference
+    "}" @range @reference))
+  (#set! highlight.kind 2))
+
+; module block start-end
+((module_declaration
+  "module" @reference
+  name: (_) @reference
+  body: (module_body
+    "{" @range @reference
+    "}" @range @reference))
+  (#set! highlight.kind 2))
+
+; enum block start-end
+((enum_declaration
+  "enum" @reference
+  name: (_) @reference
+  body: (enum_body
+    "{" @range @reference
+    "}" @range @reference))
+  (#set! highlight.kind 2))
+
+; enum constant block start-end
+((enum_constant
+  name: (_) @reference
+  body: (class_body
+    "{" @range @reference
+    "}" @range @reference))
+  (#set! highlight.kind 2))
+
+; enum block start-end
+((class_declaration
+  "class" @reference
+  name: (_) @reference
+  body: (class_body
+    "{" @range @reference
+    "}" @range @reference))
+  (#set! highlight.kind 2))
+
+; static block start-end
+((static_initializer
+  "static" @reference
+  (block
+    "{" @range @reference
+    "}" @range @reference))
+  (#set! highlight.kind 2))
+
+; constructor block start-end
+((constructor_declaration
+  name: (_) @reference
+  body: (constructor_body
+    "{" @range @reference
+    "}" @range @reference))
+  (#set! highlight.kind 2))
+
+; record block start-end
+((record_declaration
+  "record" @reference
+  name: (_) @reference
+  body: (class_body
+    "{" @range @reference
+    "}" @range @reference))
+  (#set! highlight.kind 2))
+
+; annotation type block start-end
+((annotation_type_declaration
+  "@interface" @reference
+  name: (_) @reference
+  body: (annotation_type_body
+    "{" @range @reference
+    "}" @range @reference))
+  (#set! highlight.kind 2))
+
+; interface block start-end
+((interface_declaration
+  "interface" @reference
+  name: (_) @reference
+  body: (interface_body
+    "{" @range @reference
+    "}" @range @reference))
+  (#set! highlight.kind 2))
+
+; array initializer start-end
+((variable_declarator
+  "=" @reference
+  value: (array_initializer
+    "{" @range @reference
+    "}" @range @reference))
+  (#set! highlight.kind 2))
+
+; method block start-end
+((method_declaration
+  name: (_) @reference
+  body: (block
+    "{" @range @reference
+    "}" @range @reference))
+  (#set! highlight.kind 2))
+
+; compact constructor block start-end
+((compact_constructor_declaration
+  name: (_) @reference
+  body: (block
+    "{" @range @reference
+    "}" @range @reference))
+  (#set! highlight.kind 2))
