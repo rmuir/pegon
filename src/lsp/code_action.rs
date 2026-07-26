f5abf5523a798e80154bec4a963d8822a68abd3e
Robert Muir <rmuir@apache.org>
2026-07-26 14:33:46 -0400

    test: add test for quickfix
    More like an integration test and probably should be simplified later,
    but gotta start somewhere. Does the full path from diagnostic -> code
    action list -> resolve

--- src/lsp/code_action.rs
+++ src/lsp/code_action.rs
@@ -123,3 +123,177 @@ fn quickfix(_client: &Client, _doc: &Document, params: &CodeAction) -> Result<Te
         None => bail!("invalid code"),
     })
 }
+
+#[cfg(test)]
+mod tests {
+    use std::collections::HashMap;
+
+    use gen_lsp_types::{
+        ClientCapabilities, ClientCodeActionResolveOptions, Code, CodeAction,
+        CodeActionClientCapabilities, CodeActionContext, CodeActionKind, CodeActionParams,
+        CodeActionRequest, CodeActionResolveRequest, CodeActionResponse,
+        DiagnosticClientCapabilities, DiagnosticSeverity, DiagnosticsCapabilities,
+        DidOpenTextDocumentNotification, DidOpenTextDocumentParams, DocumentDiagnosticParams,
+        DocumentDiagnosticReport, DocumentDiagnosticRequest, InitializeParams, Message,
+        PartialResultParams, Position, Range, TextDocumentClientCapabilities,
+        TextDocumentIdentifier, TextDocumentItem, TextEdit, Uri, WorkDoneProgressParams,
+        WorkspaceEdit,
+    };
+    use indoc::indoc;
+    use serde_json::json;
+
+    use crate::lsp::test_client::TestClient;
+
+    fn capabilities() -> ClientCapabilities {
+        ClientCapabilities {
+            text_document: Some(TextDocumentClientCapabilities {
+                code_action: Some(CodeActionClientCapabilities {
+                    data_support: Some(true),
+                    resolve_support: Some(ClientCodeActionResolveOptions {
+                        properties: vec!["edit".into(), "command".into()],
+                    }),
+                    ..Default::default()
+                }),
+                diagnostic: Some(DiagnosticClientCapabilities {
+                    diagnostics_capabilities: DiagnosticsCapabilities {
+                        data_support: Some(true),
+                        ..Default::default()
+                    },
+                    ..Default::default()
+                }),
+                ..Default::default()
+            }),
+            ..Default::default()
+        }
+    }
+
+    /// Get an autofix for a swallowed exception
+    #[test]
+    fn basic() {
+        let client = TestClient::new(InitializeParams {
+            capabilities: capabilities(),
+            ..Default::default()
+        });
+        client.notify::<DidOpenTextDocumentNotification>(DidOpenTextDocumentParams {
+            text_document: TextDocumentItem {
+                uri: "file:///Foo.java".into(),
+                language_id: "java".into(),
+                version: 0,
+                text: indoc! {r#"
+                public class Foo {
+                    public void bar() {
+                        try {
+                            Integer.parseInt("foo");
+                        } catch (Exception wtf) {
+                        }
+                    }
+                }
+            "#}
+                .into(),
+            },
+        });
+        let result = client.request::<DocumentDiagnosticRequest>(DocumentDiagnosticParams {
+            text_document: TextDocumentIdentifier::new("file:///Foo.java".into()),
+            previous_result_id: None,
+            identifier: None,
+            work_done_progress_params: WorkDoneProgressParams::default(),
+            partial_result_params: PartialResultParams::default(),
+        });
+
+        let DocumentDiagnosticReport::RelatedFullDocumentDiagnosticReport(full) = result else {
+            panic!();
+        };
+
+        let diagnostics = &full.full_document_diagnostic_report.items;
+
+        // one problem
+        assert_eq!(
+            vec![gen_lsp_types::Diagnostic {
+                range: Range::new(Position::new(4, 27), Position::new(4, 30)),
+                severity: Some(DiagnosticSeverity::Information),
+                code: Some(Code::String("swallowed-exception".into())),
+                source: Some(env!("CARGO_PKG_NAME").into()),
+                message: Message::String("Unhandled caught exception: `wtf`".into()),
+                data: Some(json!({ "fix": "Indicate ignored exception with unnamed variable `_`"})),
+                ..Default::default()
+            }],
+            diagnostics.clone()
+        );
+
+        let action_list = client.request::<CodeActionRequest>(CodeActionParams {
+            text_document: TextDocumentIdentifier::new("file:///Foo.java".into()),
+            range: Range::new(Position::new(0, 0), Position::new(7, 0)),
+            context: CodeActionContext {
+                diagnostics: diagnostics.clone(),
+                only: None,
+                trigger_kind: None,
+            },
+            work_done_progress_params: WorkDoneProgressParams::default(),
+            partial_result_params: PartialResultParams::default(),
+        });
+
+        assert_eq!(
+            action_list,
+            Some(vec![
+                CodeActionResponse::CodeAction(CodeAction {
+                    title: "Indicate ignored exception with unnamed variable `_`".into(),
+                    kind: Some(CodeActionKind::QuickFix),
+                    diagnostics: Some(diagnostics.clone()),
+                    is_preferred: Some(true),
+                    disabled: None,
+                    edit: None,
+                    command: None,
+                    data: Some(json!({
+                        "uri": "file:///Foo.java",
+                        "version": 0
+                    })),
+                    tags: None,
+                }),
+                CodeActionResponse::CodeAction(CodeAction {
+                    title: "Organize Imports".into(),
+                    kind: Some(CodeActionKind::SourceOrganizeImports),
+                    diagnostics: None,
+                    is_preferred: None,
+                    disabled: None,
+                    edit: None,
+                    command: None,
+                    data: Some(json!({
+                        "uri": "file:///Foo.java",
+                        "version": 0
+                    })),
+                    tags: None
+                })
+            ])
+        );
+
+        let resolved = client.request::<CodeActionResolveRequest>(CodeAction {
+            title: "Indicate ignored exception with unnamed variable `_`".into(),
+            kind: Some(CodeActionKind::QuickFix),
+            diagnostics: Some(diagnostics.clone()),
+            is_preferred: Some(true),
+            disabled: None,
+            edit: None,
+            command: None,
+            data: Some(json!({
+                "uri": "file:///Foo.java",
+                "version": 0
+            })),
+            tags: None,
+        });
+
+        assert_eq!(
+            resolved.edit,
+            Some(WorkspaceEdit {
+                changes: Some(HashMap::from([(
+                    Uri("file:///Foo.java".into()),
+                    vec![TextEdit {
+                        range: Range::new(Position::new(4, 27), Position::new(4, 30)),
+                        new_text: "_".into()
+                    }]
+                )])),
+                document_changes: None,
+                change_annotations: None,
+            }),
+        );
+    }
+}
