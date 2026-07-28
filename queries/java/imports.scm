93dbcedd4ca2a0980311439c58f9fd425ba01718
Robert Muir <rmuir@apache.org>
2026-07-28 09:00:01 -0400

    feat: organize imports
    Offer quickfix and source action to organize the imports to the google
    style. There are "groups" which are the primary sort, then the text of
    the import is the secondary sort.
    
    Import's "range" (for the purpose of sticking comments with it) extends
    to the end of the line. Comment preceding the import on its own line
    sticks with it as well.

---
+++ queries/java/imports.scm
@@ -0,0 +1,50 @@
+; Each pattern represents an import "group" in order
+; Some of these groups aren't allowed in the google style but we give them an order anyway
+; The group is the primary sort, and the @text is the secondary sort
+; ---
+; module imports first
+(program
+  (import_declaration
+    "module"
+    [
+      (identifier)
+      (scoped_identifier)
+    ] @text) @node)
+
+; wildcard static imports
+(program
+  (import_declaration
+    "static"
+    [
+      (identifier)
+      (scoped_identifier)
+    ] @text
+    (asterisk)) @node)
+
+; static imports
+(program
+  (import_declaration
+    "static"
+    [
+      (identifier)
+      (scoped_identifier)
+    ] @text .) @node)
+
+; wildcard imports
+((program
+  (import_declaration
+    [
+      (identifier)
+      (scoped_identifier)
+    ] @text
+    (asterisk)) @node)
+  (#not-match? @node "^import\\s+static"))
+
+; imports
+((program
+  (import_declaration
+    [
+      (identifier)
+      (scoped_identifier)
+    ] @text .) @node)
+  (#not-match? @node "^import\\s+static"))
---
+++ src/support/organize_imports.rs
@@ -0,0 +1,112 @@
+use anyhow::{Context as _, Result, anyhow};
+use bstr::ByteSlice as _;
+use std::ops::Range;
+use std::str::from_utf8;
+use std::sync::LazyLock;
+use tree_sitter::{Query, QueryCursor, StreamingIterator as _, Tree};
+
+use crate::support::fix::Edit;
+use crate::support::queries::capture_id;
+
+struct Import {
+    /// type of the import, primary sort key
+    pattern: usize,
+    /// text of the import, secondary sort key
+    text: Range<usize>,
+    /// range of the import, including attached comments
+    range: Range<usize>,
+}
+
+#[expect(clippy::indexing_slicing, reason = "for another day")]
+#[expect(clippy::map_err_ignore, reason = "because the error sucks!")]
+pub fn organize(tree: &Tree, data: &[u8]) -> Result<Option<Edit>> {
+    let mut imports = imports(tree, data)?;
+    // no imports or only one: already sorted
+    if imports.len() < 2 {
+        return Ok(None);
+    }
+    // create range of entire edit
+    let start_offset = imports.first().context("exists")?.range.start;
+    let end_offset = imports.last().context("exists")?.range.end;
+    // sort the imports
+    imports.sort_unstable_by(|left, right| {
+        left.pattern
+            .cmp(&right.pattern)
+            .then_with(|| data[left.text.clone()].cmp(&data[right.text.clone()]))
+    });
+    let ranges: Result<Vec<_>, _> = imports
+        .iter()
+        .map(|import| from_utf8(&data[import.range.clone()]).map_err(|_| anyhow!("invalid utf-8")))
+        .collect();
+    Ok(Some(Edit {
+        range: start_offset..end_offset,
+        replacement: ranges?.concat(),
+    }))
+}
+
+/// returns list of import ranges
+fn imports(tree: &Tree, data: &[u8]) -> Result<Vec<Import>> {
+    let mut imports = Vec::new();
+    let mut cursor = QueryCursor::new();
+
+    let mut matches = cursor.matches(&QUERY, tree.root_node(), data);
+    let mut last_end_offset = 0;
+    while let Some(hit) = matches.next() {
+        let node = hit
+            .nodes_for_capture_index(*NODE_CAPTURE)
+            .next()
+            .context("node capture should exist")?;
+
+        let text = hit
+            .nodes_for_capture_index(*TEXT_CAPTURE)
+            .next()
+            .context("text capture should exist")?;
+
+        let mut start_offset = node.byte_range().start;
+        let mut end_offset = node.byte_range().end;
+
+        // extend nodes start to include preceding comment
+        // TODO: loop here to preserve multi-line comments?
+        if let Some(sibling) = node.prev_named_sibling()
+            && sibling.byte_range().start >= last_end_offset
+            && (sibling.kind() == "line_comment" || sibling.kind() == "block_comment")
+        {
+            start_offset = sibling.byte_range().start;
+        }
+
+        // extend node's end until the end of the line
+        if let Some(newline) = data.get(end_offset..).context("valid range")?.find(b"\n") {
+            end_offset = end_offset
+                .checked_add(newline.checked_add(1).context("valid offset")?)
+                .context("valid offset")?;
+        }
+
+        // record where this node ended
+        last_end_offset = end_offset;
+
+        imports.push(Import {
+            pattern: hit.pattern_index,
+            text: text.byte_range(),
+            range: start_offset..end_offset,
+        });
+    }
+    Ok(imports)
+}
+
+/// compiled query that matches all import patterns
+static QUERY: LazyLock<Query> = LazyLock::new(|| {
+    Query::new(
+        &crate::support::language(),
+        include_str!(concat!(
+            env!("CARGO_MANIFEST_DIR"),
+            "/queries/java/imports.scm"
+        )),
+    )
+    .expect("query should compile")
+});
+
+/// index of the `@node` capture
+static NODE_CAPTURE: LazyLock<u32> = LazyLock::new(|| capture_id(&QUERY, "node"));
+
+/// index of the `@text` capture
+static TEXT_CAPTURE: LazyLock<u32> = LazyLock::new(|| capture_id(&QUERY, "text"));
