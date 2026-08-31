7cd3d356b3b8db82b5438b266d8841bd7b4f685c
Robert Muir <rmuir@apache.org>
2026-08-30 20:11:41 -0400

    refactor: current state into a struct
    This doesn't make things prettier but it will allow refactoring of the
    big function.

--- src/support/formatting.rs
+++ src/support/formatting.rs
@@ -73,18 +73,7 @@ impl JavaFormatter {
                 true
             });
 
-        // current indentation LEVEL.
-        let mut current_indent: u32 = 0;
-        // previous iterated node id, to only handle a node with one pattern
-        let mut previous_node_id = usize::MAX;
-        // previous line (row)
-        let mut previous_line = usize::MAX;
-        // if the previous terminal node was a comment node
-        let mut previous_comment = false;
-        // if there's a pending space from after the previous node
-        let mut pending_space = false;
-        // if there's a pending newline from after the previous node
-        let mut pending_newline = false;
+        let mut state = GroupState::new();
         // temporary per "line" buffer
         let mut buffer = Vec::with_capacity(256);
 
@@ -101,32 +90,39 @@ impl JavaFormatter {
 
             // first pattern wins
             let node_id = node.id();
-            if node_id == previous_node_id {
+            if node_id == state.previous_node_id {
                 continue;
             }
-            previous_node_id = node_id;
+            state.previous_node_id = node_id;
 
             let pattern = pattern(hit.pattern_index);
 
             // let comments be "sticky" / chain on the same line
-            if (pattern.comment || previous_comment)
-                && pending_newline
-                && node.start_position().row == previous_line
+            if (pattern.comment || state.previous_comment)
+                && state.pending_newline
+                && node.start_position().row == state.previous_line
             {
-                pending_newline = false;
-                pending_space = true;
+                state.pending_newline = false;
+                state.pending_space = true;
             }
-            previous_comment = pattern.comment;
+            state.previous_comment = pattern.comment;
 
             // adjust indent before node
-            current_indent = current_indent
+            state.current_indent = state
+                .current_indent
                 .checked_add_signed(pattern.indent_before.into())
                 .context("no overflow")?;
 
             // write newline before, if required
-            if !buffer.is_empty() && (pattern.newline_before || pending_newline) {
+            if !buffer.is_empty() && (pattern.newline_before || state.pending_newline) {
                 // preserve existing blank line separators
-                if pending_newline && node.start_position().row.saturating_sub(previous_line) > 1 {
+                if state.pending_newline
+                    && node
+                        .start_position()
+                        .row
+                        .saturating_sub(state.previous_line)
+                        > 1
+                {
                     buffer.extend_from_slice(self.newline);
                 }
                 buffer.extend_from_slice(self.newline);
@@ -136,7 +132,8 @@ impl JavaFormatter {
 
             // write any indent/space before
             if buffer.is_empty() {
-                let indent = current_indent
+                let indent = state
+                    .current_indent
                     .checked_mul(self.indent_size.into())
                     .context("no overflow")?;
                 buffer.resize(
@@ -146,37 +143,68 @@ impl JavaFormatter {
                         .context("no overflow")?,
                     b' ',
                 );
-            } else if pattern.space_before || pending_space {
+            } else if pattern.space_before || state.pending_space {
                 buffer.resize(buffer.len().checked_add(1).context("no overflow")?, b' ');
             }
 
             // write the node
             let bytes = data.get(node.byte_range()).context("valid range")?;
             buffer.extend_from_slice(bytes);
-            pending_space = false;
-            pending_newline = false;
+            state.pending_space = false;
+            state.pending_newline = false;
 
             // write newline/space after, if required
             if pattern.space_after {
-                pending_space = true;
+                state.pending_space = true;
             } else if pattern.newline_after {
-                pending_newline = true;
-                previous_line = node.end_position().row;
+                state.pending_newline = true;
+                state.previous_line = node.end_position().row;
             }
 
             // adjust indent after node
-            current_indent = current_indent
+            state.current_indent = state
+                .current_indent
                 .checked_add_signed(pattern.indent_after.into())
                 .context("no overflow")?;
         }
 
         // write any final pending newline
-        if !buffer.is_empty() && pending_newline {
+        if !buffer.is_empty() && state.pending_newline {
             buffer.extend_from_slice(self.newline);
             newdata.extend_from_slice(&buffer);
         }
 
         Ok(())
+    }
+}
+
+/// state per "group"
+/// should fit on one line if possible.
+struct GroupState {
+    /// current indentation LEVEL.
+    current_indent: u32,
+    /// previous iterated node id, to only handle a node with one pattern
+    previous_node_id: usize,
+    /// previous line (row)
+    previous_line: usize,
+    /// if the previous terminal node was a comment node
+    previous_comment: bool,
+    /// if there's a pending space from after the previous node
+    pending_space: bool,
+    /// if there's a pending newline from after the previous node
+    pending_newline: bool,
+}
+
+impl GroupState {
+    const fn new() -> Self {
+        Self {
+            current_indent: 0,
+            previous_node_id: usize::MAX,
+            previous_line: usize::MAX,
+            previous_comment: false,
+            pending_space: false,
+            pending_newline: false,
+        }
     }
 }
 
